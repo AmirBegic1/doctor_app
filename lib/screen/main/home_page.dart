8f91f8eae78fc25a8e595a6a8420f5fbe482cef3
Amir Begić <amir.begic2019@size.ba>
2022-12-04 04:19:31 +0100

    home page UI

--- lib/screen/main/home_page.dart
+++ lib/screen/main/home_page.dart
@@ -10,6 +10,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    var widthDevice = MediaQuery.of(context).size.width;
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
@@ -32,6 +33,200 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
           backgroundColor: Colors.white,
           elevation: 0,
+        ),
+
+        //body
+        body: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, top: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      const CircleAvatar(
+                        radius: 60,
+                        backgroundImage: AssetImage("assets/doca.png"),
+                      ),
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 10, horizontal: 20),
+                              child: const Text(
+                                "My profile",
+                                style: TextStyle(
+                                    fontSize: 17, color: Colors.black),
+                              ),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 5, horizontal: 20),
+                              child: const Text(
+                                "Dr. Jon Doe",
+                                style: TextStyle(
+                                    fontSize: 25,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 50),
+            const Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Text(
+                "Patient list for today",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18),
+              ),
+            ),
+            const SizedBox(height: 40),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Wrap(
+                    direction: Axis.vertical,
+                    spacing: 20.0,
+                    children: const [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundImage: AssetImage("assets/Rectangle 6.png"),
+                      ),
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundImage: AssetImage("assets/drugi.png"),
+                      ),
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundImage: AssetImage("assets/treci.png"),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(width: 30.0),
+                  Wrap(
+                    direction: Axis.vertical,
+                    spacing: 50.0,
+                    children: const [
+                      Text(
+                        "Alen K. \n16:00 - Common cold",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14),
+                      ),
+                      Text(
+                        "Amy F. \n13:30 Right Arm Pain",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14),
+                      ),
+                      Text(
+                        "Kim H. \n16:30 - Covid 19",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 50),
+            const Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Text(
+                "Tomorrow",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18),
+              ),
+            ),
+            const SizedBox(height: 40),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Wrap(
+                    direction: Axis.vertical,
+                    spacing: 20.0,
+                    children: const [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundImage: AssetImage("assets/andy.png"),
+                      ),
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundImage: AssetImage("assets/bell.png"),
+                      ),
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundImage: AssetImage("assets/fiona.png"),
+                      ),
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundImage: AssetImage("assets/nezir.png"),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(width: 30.0),
+                  Wrap(
+                    direction: Axis.vertical,
+                    spacing: 50.0,
+                    children: const [
+                      Text(
+                        "Andy A. \n08:00 - Common cold",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14),
+                      ),
+                      Text(
+                        "Bell B.. \n09:30 Deadache",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14),
+                      ),
+                      Text(
+                        "Fiona L. \n10:10 - Covid 19",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14),
+                      ),
+                      Text(
+                        "Nezir B. \n11:00 - Broken Heart",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
