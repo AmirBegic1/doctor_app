9f41a624449b6f0ef2dcbade7d8166c1be07f51e
Amir Begić <amir.begic2019@size.ba>
2022-12-04 02:52:14 +0100

    login screen UI

---
+++ lib/screen/auth/login_screen.dart
@@ -0,0 +1,146 @@
+import 'package:doctor_app/screen/main/home_page.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SafeArea(
+          child: Container(
+            alignment: Alignment.center,
+            child: SingleChildScrollView(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      "assets/ProductArena_logo 1.png",
+                      width: 350,
+                    ),
+                    const SizedBox(height: 30),
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 20),
+                      child: TextFormField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          suffixIcon: const Icon(
+                            Icons.email,
+                            color: Colors.black,
+                          ),
+                          iconColor: Colors.black,
+                          labelStyle: const TextStyle(color: Colors.grey),
+                          labelText: "Email",
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide: const BorderSide(
+                              color: Colors.grey,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide: const BorderSide(
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        validator: ((value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Ovo polje ne moze biti prazno!';
+                          } else if (value != "career@tech397.com") {
+                            return 'Pogrešan email';
+                          }
+                          return null;
+                        }),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 20),
+                      child: TextFormField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          suffixIcon: const Icon(
+                            Icons.remove_red_eye,
+                            color: Colors.black,
+                          ),
+                          iconColor: Colors.black,
+                          labelStyle: const TextStyle(color: Colors.grey),
+                          labelText: "Password",
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide: const BorderSide(
+                              color: Colors.grey,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide: const BorderSide(
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        validator: ((value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Ovo polje ne moze biti prazno!';
+                          } else if (value != "Pass123!") {
+                            return 'Pogrešan password';
+                          }
+                          return null;
+                        }),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('Uspjesno logirani!'),
+                              ),
+                            );
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green,
+                            minimumSize: const Size.fromHeight(8),
+                            padding: const EdgeInsets.all(8)),
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 50, vertical: 10),
+                          child: const Text(
+                            'Log in',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
